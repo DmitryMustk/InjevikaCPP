976b15739f1170c8fc41bef44a22fe5fc4fa8259
chachuher <146207091+chachuher@users.noreply.github.com>
2023-10-13 15:43:21 +0700

    Update main.cpp

--- Dima/task1/src/main.cpp
+++ Dima/task1/src/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <locale>
 
-int main(void) {
-    std::cout << "Hello, world!" << '\n';
-    return 0;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    int x;
+    cout << "Введите, пожалуйста целое число: ";
+    cin >> x;
+    switch(x)
+    {
+        case 1:
+            cout << "Январь" << "\n";
+            break;
+        case 2:
+            cout << "Февраль" << "\n";
+            break;
+        case 3:
+            cout << "Март" << "\n";
+            break;
+        case 4:
+            cout << "Апрель" << "\n";
+            break;
+        case 5:
+            cout << "Май" << "\n";
+            break;
+        case 6:
+            cout << "Июнь" << "\n";
+            break;
+        case 7:
+            cout << "Июль" << "\n";
+            break;
+        case 8:
+            cout << "Август" << "\n";
+            break;
+        case 9:
+            cout << "Сентябрь" << "\n";
+            break;
+        case 10:
+            cout << "Октябрь" << "\n";
+            break;
+        case 11:
+            cout << "Ноябрь" << "\n";
+            break;
+        case 12:
+            cout << "Декабрь" << "\n";
+            break;
+        default:
+            cout << "Нет месяца с таким номером" << "\n";
+            break;
+    }
 }
