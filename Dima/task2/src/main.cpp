0d6f4a6214d46876f999167f11522908752b5c42
chachuher <146207091+chachuher@users.noreply.github.com>
2023-10-13 15:43:54 +0700

    Update main.cpp

--- Dima/task2/src/main.cpp
+++ Dima/task2/src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
-int main(void) {
-  std::cout << "Bubble sort" << '\n';
-  return 0;
+using namespace std;
+
+int main(void)
+{
+    int a[25];
+    int x;
+    for (int i = 0; i < 25; i++) {
+        a[i] = rand() % 100;
+        cout << a[i] << ' ';
+    }
+    cout << '\n';
+    for (int i = 0; i < 24; i++) {
+        for (int j = 1; j < 25; j++) {
+            if (a[j - 1] > a[j]) {
+                x = a[j - 1];
+                a[j - 1] = a[j];
+                a[j] = x;
+            } 
+        }
+    }
+    cout << "Ready array:" << '\n';
+    for (int i = 0; i < 25; i++) {
+        cout << a[i] << ' ';
+    }
+    return 0;
 }
