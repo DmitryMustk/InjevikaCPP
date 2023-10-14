df073d17deb4f06ec7955b9ccb4583f7f6ede975
VAN0-M <146544702+VAN0-M@users.noreply.github.com>
2023-10-14 20:26:52 +0700

    Update main.cpp

--- Vanya/task2/src/main.cpp
+++ Vanya/task2/src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <random>
+using namespace std;
 
-int main(void) {
-  std::cout << "Bubble sort" << '\n';
-  return 0;
+#define size 10
+#define rand_max 100
+
+int main(void)
+{
+	int a[size], buf = 0;
+	srand(time(0));
+	for (int i = 0; i < size; i++)
+		a[i] = rand() % rand_max;
+
+	for (int i = 0; i < size; i++)
+		cout << a[i] << ' ';
+	cout << '\n';
+
+	for (int j = 1; j < size; j++)
+	{
+		for (int i = 0; i < size - 1; i++)
+		{
+			if (a[i] > a[i + 1])
+			{
+				buf = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = buf;
+			}	
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+		cout << a[i] << ' ';
+	cout << '\n';
+
+	return 0;
 }
