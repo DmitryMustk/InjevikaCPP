4d221009bfb7d148da3aa8934deef973e7ea9a30
VAN0-M <146544702+VAN0-M@users.noreply.github.com>
2023-10-14 20:25:07 +0700

    Update main.cpp

--- Vanya/task1/src/main.cpp
+++ Vanya/task1/src/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+using namespace std;
 
-int main(void) {
-    std::cout << "Hello, world!" << '\n';
-    return 0;
+int main(void)
+{
+	int a;
+	cout << "Write number of month:" << endl;
+	cin >> a;
+	if (a > 12 || a < 1)
+		cout << "ERROR!" << endl;
+	else
+	{
+		switch (a)
+		{
+		    case 1:
+			cout << "January" << endl;
+			break;
+
+			case 2:
+			cout << "February" << endl;
+			break;
+
+			case 3:
+			cout << "March" << endl;
+			break;
+
+			case 4:
+			cout << "April" << endl;
+			break;
+
+			case 5:
+			cout << "May" << endl;
+			break;
+
+			case 6:
+			cout << "June" << endl;
+			break;
+
+			case 7:
+			cout << "July" << endl;
+			break;
+
+			case 8:
+			cout << "August" << endl;
+			break;
+
+			case 9:
+			cout << "September" << endl;
+			break;
+
+			case 10:
+			cout << "October" << endl;
+			break;
+
+			case 11:
+			cout << "November" << endl;
+			break;
+
+			case 12:
+			cout << "December" << endl;
+			break;
+		}
+
+	}
+	return 0;
 }
